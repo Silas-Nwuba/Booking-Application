1b940f51f6c54d58fdbf1bc02a4a9f862391c8a0
ebukanwuba45@gmail.com <ebukanwuba45@gmail.com>
2022-06-16 12:55:39 +0100

    working on the destination page

--- Controllers/DestinationController.cs
+++ Controllers/DestinationController.cs
@@ -1,4 +1,5 @@
 ﻿using BookingApplication.Models;
+using DivineProjectPersistence;
 using DivineProjectServices.ProjectServices;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,17 +13,21 @@ namespace BookingApplication.Controllers
     public class DestinationController : Controller
     {
         private readonly IDestination _destination;
+        private readonly ISchedule _schedule;
+        private readonly ApplicationDbContext _Context;
 
-        public DestinationController(IDestination destination)
+        public DestinationController(IDestination destination, ApplicationDbContext context, ISchedule schedule)
         {
             _destination = destination;
+            _Context = context;
+            _schedule = schedule;
         }
-
         // GET: DestinationController
         public ActionResult Index()
         {
             ViewBag.Depart = _destination.GetAllDeparture();
             ViewBag.Arival = _destination.GetAllArival();
+            ViewBag.DateTime = _destination.GetAllDateTime();
             var ViewModel = new DestinationViewModel();
             return View(ViewModel);
         }
@@ -32,28 +37,42 @@ namespace BookingApplication.Controllers
         {
             return View();
         }
-
         // GET: DestinationController/Create
-        public ActionResult Create()
+        [HttpGet]
+        public ActionResult Create(int DepartureId,int ArivalToId , int DateofDeparture, int NoOfPassenger)
         {
+            if(DepartureId != 0 &&  ArivalToId != 0)
+            {
+                var ViewModel = _Context.Travellings.Where(c => c.DepartureId == DepartureId && c.ArivalToId == ArivalToId).SingleOrDefault();
+                var Model = new DestinationCreateViewModel()
+                {
+                    DepartureFrom = _Context.Departures.FirstOrDefault(x => x.Id == DepartureId).DepartureFrom,
+                    ArivalTo = _Context.Arivals.FirstOrDefault(c => c.Id == ArivalToId).Arival,
+                    DateAndTime = _Context.DateAndTimes.FirstOrDefault(c => c.Id == DateofDeparture).Date,
+                    Passenager = NoOfPassenger,
+                    Time = _Context.DateAndTimes.FirstOrDefault(c => c.Id == DateofDeparture).Time,
+                    BusName = _Context.buses.FirstOrDefault(c => c.Id == ViewModel.BusId).BusName,
+                    SeatCapacity = _Context.buses.FirstOrDefault(c => c.Id == ViewModel.BusId).SeatCapacity,
+                    Price = ViewModel.Price,
+                };
+                return View(Model);
+            }
             return View();
         }
-
-        // POST: DestinationController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
+       // POST: DestinationController/Create
+       //[HttpPost]
+       //[ValidateAntiForgeryToken]
+       // public ActionResult Create(IFormCollection collection)
+       // {
+       //     try
+       //     {
+       //         return RedirectToAction(nameof(Index));
+       //     }
+       //     catch
+       //     {
+       //         return View();
+       //     }
+       // }
         // GET: DestinationController/Edit/5
         public ActionResult Edit(int id)
         {
--- Models/DestinationCreateViewModel.cs
+++ Models/DestinationCreateViewModel.cs
@@ -6,26 +6,27 @@ using System.Threading.Tasks;
 
 namespace BookingApplication.Models
 {
-    public class DestinationViewModel
+    public class DestinationCreateViewModel
     {
         public int Id { get; set; }
+      
         [Required(ErrorMessage = "Departure field is required")]
         public int DepartureId { get; set; }
-        [Required(ErrorMessage = "Arival To field is required")]
         public int ArivalToId { get; set; }
-        
+        [Required(ErrorMessage = "Arival To field is required")]
         [Display(Name = "Arival To")]
         public string ArivalTo { get; set; }
-      
+        [Required]
         [Display(Name = "Departure From")]
         public string DepartureFrom { get; set; }
-        [Display(Name = "Date of Departure")]
-        public string DateofDeparture { get; set; }
-        public int NoOfPassenger { get; set; } = 1;
-
+        public string  Seat { get; set; }
+        [Display(Name = "Total Price")]
         public decimal Price { get; set; }
         [Display(Name = "Bus Name")]
         public string BusName { get; set; }
         public int SeatCapacity { get; set; }
+        public string DateAndTime { get;  set; }
+        public string Time { get; set; }
+        public int Passenager { get; set; }
     }
 }
